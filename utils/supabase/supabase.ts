ab77b0a32878af6725f6d009d9d33823b7d61921
N. David Bello <bell0465@flinders.edu.au>
2025-01-20 21:36:24 +1030

    Adjustment of environment variables for Supabase

--- utils/supabase/supabase.ts
+++ utils/supabase/supabase.ts
@@ -1,6 +1,6 @@
 ﻿import {createClient} from "@supabase/supabase-js";
 
 const supabaseUrl: string | undefined = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey : string | undefined = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseKey : string | undefined = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl!, supabaseKey!);
