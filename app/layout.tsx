7f752dc84974e6c609a7881fc34e5437ed3df9b4
N. David Bello <bell0465@flinders.edu.au>
2025-01-22 12:15:58 +1030

    Inclusion of image for metada

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,24 @@ import { Footer } from "../components/Footer/Footer";
 export const metadata = {
   title: "Launch Online SA",
   description: "Initiative to help small businesses in SA to launch online",
+  openGraph: {
+    title: "Launch Online SA",
+    description: "Initiative to help small businesses in SA to launch online",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Launch Online SA",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Launch Online SA",
+    description: "Initiative to help small businesses in SA to launch online",
+    images: ["/og-image.png"],
+  },
 };
 
 const inter = Inter({
